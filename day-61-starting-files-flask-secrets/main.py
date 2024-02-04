b37f40ae05adaaf3e25fc47f2bf4571ef0394935
krizhnaa <krizhnaprem@gmail.com>
2024-02-04 17:53:09 +0530

    Email and password validators are tested

--- day-61-starting-files-flask-secrets/main.py
+++ day-61-starting-files-flask-secrets/main.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, validators
+from wtforms.validators import DataRequired
+
+
+class MyForm(FlaskForm):
+    email = StringField(label='E-mail', validators=[
+        validators.Length(min=10, message=('Little short for an email address?')),
+        validators.Email(message=('That\'s not a valid email address.'))
+    ])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label="Log In")
+
 
 app = Flask(__name__)
+app.secret_key = "lolol"
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = MyForm()
+    form.validate_on_submit()
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
