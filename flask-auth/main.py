2eb252ee51f3bf22eb8a05521e1190081acae56b
krizhnaa <krizhnaprem@gmail.com>
2024-02-11 20:39:03 +0530

    End of Session

--- flask-auth/main.py
+++ flask-auth/main.py
@@ -1,15 +1,12 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
 
-# CREATE DATABASE
 class Base(DeclarativeBase):
     pass
 
